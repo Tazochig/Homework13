9295a644ec5f8c9ec740139ddfcdc5c43ee2a41b
Tazochig <chigladzetazo@gmail.com>
2024-10-18 06:03:33 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+// console.log
+// console.warn
+// console.error
+// console.info
+// let
+// var
+// const position
+let Name = "Tazo";
+
+let LastName = " Chighladze";
+
+let saxeligvari = Name + LastName ;
+
+console.log(saxeligvari);
+
+let Age = "22";
+
+console.log(Age);
+
+let Mail = "Chigladzetazo@gmail.com";
+
+console.log(Mail);
+
+let Number = "593429614";
+
+console.error(Number)
+
+let asakicheck = "სრულწლოვანია"
+
+console.warn(asakicheck)
+
+// Me on JS be like :🤯
